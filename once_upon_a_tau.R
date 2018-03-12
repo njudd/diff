d2cc440c6b2f95bcf7df310bda788bfaedf3f3ae
njudd <nickkjudd@gmail.com>
2018-03-12 09:52:19 +0100

    having issues, can't reliably see a exgaus distro, maybe to few trials

--- once_upon_a_tau.R
+++ once_upon_a_tau.R
@@ -1,3 +1,13 @@
+#tau for WM_grid
+
+#cut outliers <100000 (must be a glitch)
+## be carefull, the distributions aren't ex-gaussian** 
+# check other tasks, make a lm controlling for lv? or somehow get rid of lv
+
+
+table(x$Phase.Type) # whats the difference, If i remember correctly this df is clean
+
+
 setwd("~/Dropbox/KI/Diff_project/Grid_diff")
 x <- read.csv("WMgrid_training_full_pure.csv")
 keep <-x
